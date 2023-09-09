676e85f62939afa1b9b9ba4b08e38e6ccb9612d8
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-09-09 18:57:26 +0300

    Create UI for 1and1, 3m, Acrom, adi and Adobe scrapers

--- js/scrapers.js
+++ js/scrapers.js
@@ -2,4 +2,16 @@ let scrapers = [
   {
     name: "1and1",
   },
+  {
+    name: "3m",
+  },
+  {
+    name: "acrom",
+  },
+  {
+    name:"adi"
+  },
+  {
+    name:"adobe"
+  }
 ];
---
+++ src/1and1/script.js
@@ -0,0 +1,40 @@
+const apiObj = {
+  file: "1and1.py",
+};
+
+// fetch api
+const fetchApi = async (apiObj) => {
+  const response = await fetch(
+    "http://localhost:8000/scraper/based_scraper_py/",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiObj),
+    }
+  );
+  const data = await response.json();
+  return data;
+};
+
+const button = document.querySelector("button");
+const svg = document.querySelector("svg");
+const div = document.querySelector("div");
+const a = document.querySelector("a");
+
+button.addEventListener("click", () => {
+  svg.classList.toggle("rotate");
+  button.disabled = true;
+  fetchApi(apiObj).then((data) => {
+    svg.classList.toggle("rotate");
+    button.disabled = false;
+    console.log(data);
+    if (data.succes) {
+      document.querySelector("#status").innerHTML = "Active";
+      document.querySelector("#jobs").innerHTML = data.Total;
+    } else {
+      document.querySelector("#status").innerHTML = "Inactive";
+    }
+  });
+});
