e0108ad89286777ba3b8f6284c5c53fa5298dd44
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-09-19 12:11:52 +0300

    fixed

--- src/cbre/script.js
+++ src/cbre/script.js
@@ -1,11 +1,23 @@
+// De Modificat
 const apiObj = {
   file: "cbre.js",
+  url: "https://dev.laurentiumarian.ro/scraper/based_scraper_js/",
 };
+// ##########################################
+
+const companyName = apiObj.file.split(".")[0];
+const capitelizedCompanyName =
+  companyName.charAt(0).toUpperCase() + companyName.slice(1);
+
+const htmlTitle = document.querySelector("title");
+const company = document.querySelector("#company");
+htmlTitle.innerHTML = `Scraper-${capitelizedCompanyName}`;
+company.innerHTML = capitelizedCompanyName;
 
 // fetch api
 const fetchApi = async (apiObj) => {
   const response = await fetch(
-    "https://dev.laurentiumarian.ro/scraper/based_scraper_js/",
+    apiObj.url,
     {
       method: "POST",
       headers: {
@@ -19,35 +31,39 @@ const fetchApi = async (apiObj) => {
 };
 
 const validate_data = (data, keyword) => {
+  let isValidate = false;
   if (
     data[keyword] !== undefined &&
     data[keyword] !== null &&
     data[keyword] !== ""
   ) {
-    return true;
+    isValidate = true;
   }
-  return false;
+  return isValidate;
 };
 
 const validate_link = (data, keyword) => {
+  let isValidate = false;
   if (
     data[keyword] !== undefined &&
     data[keyword] !== null &&
     data[keyword] !== "" &&
     data[keyword].includes("https://")
   ) {
-    return true;
+    isValidate = true;
   }
-  return false;
+  return isValidate;
 };
 
 const validate_country = (data, keyword) => {
-  for (let i = 0; i < countries.length; i++) {
-    if (countries[i].name.toLowerCase().includes(data[keyword].toLowerCase())) {
-      return true;
+  let isValidate = false;
+  for (let city of countries) {
+    if (city.name.toLowerCase().includes(data.country.toLowerCase())) {
+      isValidate = true;
+      break;
     }
   }
-  return false;
+  return isValidate;
 };
 
 const create_job = (data) => {
@@ -66,28 +82,25 @@ const create_job = (data) => {
           ${validate_data(data, "company") ? data.company : "No company"}
       </div>
       <div class="job-location">
-      
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                  <path d="M17.5 8.33334C17.5 14.1667 10 19.1667 10 19.1667C10 19.1667 2.5 14.1667 2.5 8.33334C2.5 6.34421 3.29018 4.43656 4.6967 3.03003C6.10322 1.62351 8.01088 0.833336 10 0.833336C11.9891 0.833336 13.8968 1.62351 15.3033 3.03003C16.7098 4.43656 17.5 6.34421 17.5 8.33334Z" stroke="#979C9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                  <path d="M10 10.8333C11.3807 10.8333 12.5 9.71405 12.5 8.33334C12.5 6.95262 11.3807 5.83334 10 5.83334C8.61929 5.83334 7.5 6.95262 7.5 8.33334C7.5 9.71405 8.61929 10.8333 10 10.8333Z" stroke="#979C9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
-              <div class="${
-                validate_data(data, "city") ? "validate" : "invalid"
-              }"
-              }">
-                  ${validate_data(data, "city") ? data.city : "No city"}
-              </div>,
-              <div class="${
-                validate_country(data, "country") ? "validate" : "invalid"
-              }">
-                  ${
-                    validate_country(data, "country")
-                      ? data.country
-                      : data.country + " is not a country"
-                  }
-              </div> 
-            
-        </div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                <path d="M17.5 8.33334C17.5 14.1667 10 19.1667 10 19.1667C10 19.1667 2.5 14.1667 2.5 8.33334C2.5 6.34421 3.29018 4.43656 4.6967 3.03003C6.10322 1.62351 8.01088 0.833336 10 0.833336C11.9891 0.833336 13.8968 1.62351 15.3033 3.03003C16.7098 4.43656 17.5 6.34421 17.5 8.33334Z" stroke="#979C9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M10 10.8333C11.3807 10.8333 12.5 9.71405 12.5 8.33334C12.5 6.95262 11.3807 5.83334 10 5.83334C8.61929 5.83334 7.5 6.95262 7.5 8.33334C7.5 9.71405 8.61929 10.8333 10 10.8333Z" stroke="#979C9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <div class="${
+              validate_data(data, "city") ? "validate" : "invalid"
+            }">
+                ${validate_data(data, "city") ? data.city : "No city"}
+            </div>,
+            <div class="${
+              validate_country(data, "country") ? "validate" : "invalid"
+            }">
+                ${
+                  validate_country(data, "country")
+                    ? data.country
+                    : data.country + " is not a country"
+                }
+            </div> 
+      </div>
     </div>
       <div class="job_link-container">
       <a href="${data.job_link}" class="
@@ -100,8 +113,6 @@ const create_job = (data) => {
           </svg>
       </a>
       </div>
-    
-
   `;
   document.querySelector(".jobs").appendChild(jobElement);
 };
@@ -114,12 +125,12 @@ const button = document.querySelector("button");
 const svg = document.querySelector("svg");
 
 const status = (document.querySelector("#status").innerHTML =
-  localStorage.getItem("status-cbre"));
+  localStorage.getItem(`status-${companyName}`));
 const jobsTotal = (document.querySelector("#jobs").innerHTML =
-  localStorage.getItem("jobs-cbre"));
+  localStorage.getItem(`jobs-${companyName}`));
 const lastUpdate = (document.querySelector("#last-update").innerHTML =
-  localStorage.getItem("lastUpdate-cbre"));
-const datasave = JSON.parse(localStorage.getItem("data-cbre"));
+  localStorage.getItem(`lastUpdate-${companyName}`));
+const datasave = JSON.parse(localStorage.getItem(`data-${companyName}`));
 
 const container = document.querySelector(".skeleton-jobs");
 const cardTemplate = document.getElementById("card-template");
@@ -152,13 +163,16 @@ button.addEventListener("click", () => {
         data.succes.forEach((job) => {
           create_job(job);
         });
-        localStorage.setItem("data-cbre", JSON.stringify(data.succes));
-        localStorage.setItem("status-cbre", "Active");
-        localStorage.setItem("jobs-cbre", data.Total);
-        localStorage.setItem("lastUpdate-cbre", dateTime);
+        localStorage.setItem(
+          `data-${companyName}`,
+          JSON.stringify(data.succes)
+        );
+        localStorage.setItem(`status-${companyName}`, "Active");
+        localStorage.setItem(`jobs-${companyName}`, data.Total);
+        localStorage.setItem(`lastUpdate-${companyName}`, dateTime);
       } else {
-        localStorage.setItem("status-cbre", "Inactive");
-        localStorage.setItem("lastUpdate-cbre", dateTime);
+        localStorage.setItem(`status-${companyName}`, "Inactive");
+        localStorage.setItem(`lastUpdate-${companyName}`, dateTime);
         document.querySelector("#status").innerHTML = "Inactive";
       }
     })
@@ -169,6 +183,13 @@ button.addEventListener("click", () => {
     });
 });
 
+const noDataImage = `
+<div class='no-data'>
+  <img src="https://icon-library.com/images/no-data-icon/no-data-icon-4.jpg" alt="no-data"/>
+   <p>No data in Local Storage</p>
+</div>
+`;
+
 if (datasave !== null) {
   datasave.forEach((post) => {
     create_job(post);
@@ -177,27 +198,17 @@ if (datasave !== null) {
   document.querySelector("#status").innerHTML = "Uknown";
   document.querySelector("#jobs").innerHTML = "Uknown";
   document.querySelector("#last-update").innerHTML = "Uknown";
-  document.querySelector(".jobs").innerHTML = `
-<div class='no-data'>
-  <img src="https://icon-library.com/images/no-data-icon/no-data-icon-4.jpg" alt="no-data"/>
-   <p>No data in Local Storage</p>
-</div>
-`;
+  document.querySelector(".jobs").innerHTML = noDataImage;
 }
 
 const removeLocalStorage = document.querySelector(".delete-storage");
 
 removeLocalStorage.addEventListener("click", () => {
-  localStorage.removeItem("data-cbre");
-  localStorage.removeItem("status-cbre");
-  localStorage.removeItem("jobs-cbre");
-  localStorage.removeItem("lastUpdate-cbre");
-  document.querySelector(".jobs").innerHTML = `
-<div class='no-data'>
-  <img src="https://icon-library.com/images/no-data-icon/no-data-icon-4.jpg" alt="no-data"/>
-   <p>No data in Local Storage</p>
-</div>
-`;
+  localStorage.removeItem(`data-${companyName}`);
+  localStorage.removeItem(`status-${companyName}`);
+  localStorage.removeItem(`jobs-${companyName}`);
+  localStorage.removeItem(`lastUpdate-${companyName}`);
+  document.querySelector(".jobs").innerHTML = noDataImage;
   document.querySelector("#status").innerHTML = "Uknown";
   document.querySelector("#jobs").innerHTML = "Uknown";
   document.querySelector("#last-update").innerHTML = "Uknown";
