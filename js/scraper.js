19aa3ae2d0e51c75e1897231a3bec98691e1760a
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-10-10 00:23:30 +0300

    added job type validation (remote/on-site/hibrid) and restyle job elements

--- js/scraper.js
+++ js/scraper.js
@@ -9,15 +9,14 @@ company.innerHTML = capitelizedCompanyName;
 
 const cityInRomania = (city) => {
   const cities = {
-    "bucharest": "Bucuresti",
-  }
+    bucharest: "Bucuresti",
+  };
 
   if (cities[city.toLowerCase()]) {
     return cities[city.toLowerCase()];
   }
   return city;
-}
-
+};
 
 // fetch api
 const fetchApi = async (apiObj) => {
@@ -59,51 +58,60 @@ const validate_link = (data, keyword) => {
 
 const validate_city = (data) => {
   let validatedData = [];
-  if(typeof data === "object"){  
-    data.forEach((city) => {
-      let isValidate = getTownAndCounty(
-        cityInRomania(city)
-      ).foudedTown;
-      let htmlDiv = document.createElement("div");
-      if(isValidate){
+  let citys = data.city;
+  let jobType = data.remote;
+  if (jobType === "Remote") {
+    if (typeof citys === "object") {
+      citys.forEach((city) => {
+        let htmlDiv = document.createElement("div");
         htmlDiv.classList.add("validate");
         htmlDiv.innerHTML = city;
-        validatedData.push(
-          htmlDiv
-        );
-      } else {
-        htmlDiv.classList.add("invalid");
-        htmlDiv.innerHTML = `${city} is not a city in Romania`;
-        validatedData.push(
-          htmlDiv
-        );
-      }
-    });
-  } else if (typeof data === "string") {
-    let htmlDiv = document.createElement("div");
-    let isValidate = getTownAndCounty(
-      cityInRomania(data)
-    ).foudedTown;
-    if(isValidate){
+        validatedData.push(htmlDiv);
+      });
+    } else if (typeof citys === "string") {
+      let htmlDiv = document.createElement("div");
       htmlDiv.classList.add("validate");
-      htmlDiv.innerHTML = data;
-      validatedData.push(
-        htmlDiv
-      );
-    } else {
-      htmlDiv.classList.add("invalid");
-      htmlDiv.innerHTML = `${data} is not a city in Romania`;
-      validatedData.push(
-        htmlDiv
-      );
+      htmlDiv.innerHTML = citys;
+      validatedData.push(htmlDiv);
+    }
+  } else {
+    if (typeof citys === "object") {
+      citys.forEach((city) => {
+        let isValidate = getTownAndCounty(cityInRomania(city)).foudedTown;
+        let htmlDiv = document.createElement("div");
+        if (isValidate) {
+          htmlDiv.classList.add("validate");
+          htmlDiv.innerHTML = city;
+          validatedData.push(htmlDiv);
+        } else {
+          htmlDiv.classList.add("invalid");
+          htmlDiv.innerHTML = `${city} is not a city in Romania`;
+          validatedData.push(htmlDiv);
+        }
+      });
+    } else if (typeof citys === "string") {
+      let htmlDiv = document.createElement("div");
+      let isValidate = getTownAndCounty(cityInRomania(citys)).foudedTown;
+      if (isValidate) {
+        htmlDiv.classList.add("validate");
+        htmlDiv.innerHTML = citys;
+        validatedData.push(htmlDiv);
+      } else {
+        if (citys === "") {
+          htmlDiv.classList.add("invalid");
+          htmlDiv.innerHTML = `No city`;
+          validatedData.push(htmlDiv);
+        } else {
+          htmlDiv.classList.add("invalid");
+          htmlDiv.innerHTML = `${citys} is not a city in Romania`;
+          validatedData.push(htmlDiv);
+        }
+      }
     }
   }
-  
+
   return validatedData;
-
 };
-
-console.log(validate_city("Romania"));
 
 const validate_country = (data, keyword) => {
   let isValidate = false;
@@ -120,51 +128,68 @@ const create_job = (data) => {
   let jobElement = document.createElement("div");
   jobElement.classList.add("job");
   jobElement.innerHTML = `
-        <div class="details-container">
-            <h2 class="job-title ${
-              validate_data(data, "job_title") ? "validate" : "invalid"
+    <div class="details-container">
+      <h2 class="job-title ${
+          validate_data(data, "job_title") ? "validate" : "invalid"
+        }">
+            ${
+              validate_data(data, "job_title")
+                ? data.job_title
+                : "No job title"
+            }
+      </h2>
+      <div class="job-company ${
+        validate_data(data, "company") ? "validate" : "invalid"
+      }">
+          ${validate_data(data, "company") ? data.company : "No company"}
+      </div>
+      <div class="job-location" title="Location">
+          <div class="location-container">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                <path d="M17.5 8.33334C17.5 14.1667 10 19.1667 10 19.1667C10 19.1667 2.5 14.1667 2.5 8.33334C2.5 6.34421 3.29018 4.43656 4.6967 3.03003C6.10322 1.62351 8.01088 0.833336 10 0.833336C11.9891 0.833336 13.8968 1.62351 15.3033 3.03003C16.7098 4.43656 17.5 6.34421 17.5 8.33334Z" stroke="#979C9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M10 10.8333C11.3807 10.8333 12.5 9.71405 12.5 8.33334C12.5 6.95262 11.3807 5.83334 10 5.83334C8.61929 5.83334 7.5 6.95262 7.5 8.33334C7.5 9.71405 8.61929 10.8333 10 10.8333Z" stroke="#979C9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            ${
+              validate_city(data)
+                ? validate_city(data).map((city) => city.outerHTML)
+                : "<div class='invalid'>No city</div>"
+            }
+          </div>
+          <div class="country-container" title="Country">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-globe-americas" viewBox="0 0 16 16">
+              <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484-.08.08-.162.158-.242.234-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/>
+            </svg>
+            <div class="${
+              validate_country(data, "country") ? "validate" : "invalid"
             }">
                 ${
-                  validate_data(data, "job_title")
-                    ? data.job_title
-                    : "No job title"
+                  validate_country(data, "country")
+                    ? data.country
+                    : data.country + " is not a country"
                 }
-            </h2>
-            <div class="job-company ${
-              validate_data(data, "company") ? "validate" : "invalid"
+            </div> 
+          </div>
+          <div class="job-type-container" title="Job Type">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-workspace" viewBox="0 0 16 16">
+              <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+              <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z"/>
+            </svg>
+            <div class="${
+              data.remote ? "validate" : "invalid"
             }">
-                ${validate_data(data, "company") ? data.company : "No company"}
+                ${data.remote ? "Remote" : "No Job Type"}
             </div>
-            <div class="job-location">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                        <path d="M17.5 8.33334C17.5 14.1667 10 19.1667 10 19.1667C10 19.1667 2.5 14.1667 2.5 8.33334C2.5 6.34421 3.29018 4.43656 4.6967 3.03003C6.10322 1.62351 8.01088 0.833336 10 0.833336C11.9891 0.833336 13.8968 1.62351 15.3033 3.03003C16.7098 4.43656 17.5 6.34421 17.5 8.33334Z" stroke="#979C9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M10 10.8333C11.3807 10.8333 12.5 9.71405 12.5 8.33334C12.5 6.95262 11.3807 5.83334 10 5.83334C8.61929 5.83334 7.5 6.95262 7.5 8.33334C7.5 9.71405 8.61929 10.8333 10 10.8333Z" stroke="#979C9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                    ${
-                      validate_city(data.city)
-                        ? validate_city(data.city).map((city) => city.outerHTML)
-                        : "<div class='invalid'>No city</div>"
-                    }
-                    ,
-                    <div class="${
-                      validate_country(data, "country") ? "validate" : "invalid"
-                    }">
-                        ${
-                          validate_country(data, "country")
-                            ? data.country
-                            : data.country + " is not a country"
-                        }
-                    </div> 
-            </div>
-            </div>
-            <div class="job_link-container">
-            <a href="${data.job_link}" class="
-            ${validate_link(data, "job_link") ? "validate" : "invalid"}"
-            target="_blank"
-            ">
-                Vezi Postul 
-            </a>
-            </div>
+          </div>
+      </div>
+      <div class="job_link-container">
+        <a href="${data.job_link}" class="
+          ${validate_link(data, "job_link") ? "validate" : "invalid"}"
+          target="_blank"
+          ">
+              Vezi Postul 
+        </a>
+      </div>
+    </div>
         `;
   document.querySelector(".jobs").appendChild(jobElement);
 };
