eb61047b71ecc4c9f09038124de8ccb0ac89c9bb
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2023-12-11 14:37:10 +0200

    new ui

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const cardContainer = document.querySelector(".container");
 const selectScrapere = document.querySelector(".count-scrapere");
+const companiesLength = document.querySelector(".companies-length");
+let scrapereFiltrati;
 let onpage_scrapere = [];
 let scrapere = [];
 let step = 48;
@@ -18,22 +20,82 @@ window.addEventListener("scroll", () => {
 const searchInput = document.getElementById("search");
 searchInput.addEventListener("input", (e) => {
   const dateInput = e.target.value.toLowerCase().replace(/\s+/g, "");
-  const scrapereFiltrati = onpage_scrapere.filter((scraper) =>
+  scrapereFiltrati = onpage_scrapere.filter((scraper) =>
     scraper.textContent.toLowerCase().replace(/\s+/g, "").includes(dateInput)
   );
   cardContainer.innerHTML = "";
   scrapereFiltrati.forEach((scraper) => {
     cardContainer.appendChild(scraper);
   });
+
+  if (scrapereFiltrati.length == 1) {
+    companiesLength.innerHTML = `${scrapereFiltrati.length} rezultat`;
+  } else {
+    companiesLength.innerHTML = `${scrapereFiltrati.length} rezultate`;
+  }
+
   if (scrapereFiltrati.length === 0) {
     cardContainer.innerHTML = `<h1 class="not-found">Nu am gasit nimic pentru ${dateInput} !</h1>`;
+    companiesLength.innerHTML = `${scrapereFiltrati.length} rezultate`;
   }
+});
+
+const searchToggle = document.querySelector(".search-toggle");
+const filtersHeader = document.querySelector(".header-filter-container");
+const filtersHeader2 = document.querySelector(".header-filter-container2");
+
+searchToggle.addEventListener("click", () => {
+  if (!filtersHeader.classList.contains("header-filter-show")) {
+    filtersHeader2.classList.toggle("header-filter-show2");
+    setTimeout(() => {
+      filtersHeader.classList.toggle("header-filter-show");
+    }, "800");
+  } else {
+    filtersHeader.classList.toggle("header-filter-show");
+    setTimeout(() => {
+      filtersHeader2.classList.toggle("header-filter-show2");
+    }, "100");
+  }
+});
+
+searchToggle.addEventListener("click", () => {
+  headerLinksMobile.classList.remove("header-links-show");
+  document.body.style.overflowY = null;
+});
+
+const headerLinksMobile = document.querySelector(".header-links");
+const openHeaderLinks = document.querySelector(".header-mobile");
+const closeHeaderLinks = document.querySelector(".close-header-links");
+
+openHeaderLinks.addEventListener("click", () => {
+  headerLinksMobile.classList.add("header-links-show");
+  document.body.style.overflowY = "hidden";
+});
+
+closeHeaderLinks.addEventListener("click", () => {
+  headerLinksMobile.classList.remove("header-links-show");
+  document.body.style.overflowY = null;
+});
+
+const filterMobile = document.querySelector(".header-filter-mobile");
+const headerFilterMobile = document.querySelector(".header-filters");
+const closeFilter = document.querySelector(".close-header");
+
+filterMobile.addEventListener("click", () => {
+  headerFilterMobile.classList.add("display-flex");
+  document.body.style.overflowY = "hidden";
+});
+
+closeFilter.addEventListener("click", () => {
+  headerFilterMobile.classList.remove("display-flex");
+  document.body.style.overflowY = null;
 });
 
 fetch("https://api.peviitor.ro/v1/logo/")
   .then((response) => response.json())
   .then((data) => {
-    selectScrapere.innerHTML = `${data.companies.length} companii scrapuite!`;
+    companiesLength.innerHTML = `${data.companies.length} rezultate`;
+    selectScrapere.innerHTML = `Gaseste listinguri de la ${data.companies.length} companii scrapuite`;
     scrapere = data.companies;
     displayScrapere(scrapere);
   })
@@ -69,9 +131,38 @@ function displayScrapere(scrapere) {
     scraperLink.href = "src/" + collaborator.name + "/index.html";
     scraperLink.innerHTML = "Vizualizeaza Scraper";
 
-    setTimeout(() => {
-      checkFileExist(scraperLink, scraperLink);
-    }, "1000");
+    const checkScrapers = document.querySelector(".checkScrapers");
+    const checkScrapersLoading = document.querySelector(
+      ".scraper-check-loading"
+    );
+
+    checkScrapers.addEventListener("click", () => {
+      checkScrapersLoading.classList.add("rotate");
+      localStorage.removeItem("No Scraper-ui " + collaborator.name);
+      setTimeout(() => {
+        var xhr = new XMLHttpRequest();
+        xhr.open("head", scraperLink, false);
+        xhr.send();
+        if (xhr.status == 404) {
+          scraperLink.classList.add("add-class");
+          localStorage.setItem(
+            "No Scraper-ui " + collaborator.name,
+            collaborator.name
+          );
+        } else {
+          scraperLink.classList.remove("add-class");
+        }
+      }, "1000");
+      setTimeout(() => checkScrapersLoading.classList.remove("rotate"), "4000");
+    });
+
+    const localStorageScrapers = localStorage.getItem(
+      "No Scraper-ui " + collaborator.name
+    );
+
+    if (localStorageScrapers === collaborator.name) {
+      scraperLink.classList.add("add-class");
+    }
 
     // logo companies
     const scraperLogoContainer = document.createElement("div");
@@ -92,29 +183,31 @@ function displayScrapere(scrapere) {
       scraperLogo.src = "./images/assets/logonotfound.png";
     };
 
+    const scraperLinkSite = document.createElement("a");
+    scraperLinkSite.classList.add("scraper-link-site");
+    scraperLinkSite.innerHTML = "Viziteaza Website";
+
     const scraperInfo = document.createElement("div");
     scraperInfo.classList.add("scraper-info");
     const scraperName = document.createElement("div");
     scraperName.classList.add("scraper-name");
     const name = document.createElement("h2");
     name.innerText = collaborator.name;
+    const scraperDescription = document.createElement("p");
+    scraperDescription.classList.add("scraper-description");
+    scraperDescription.innerHTML =
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus sint eligendi ";
     scraperName.appendChild(name);
     scraperInfo.appendChild(scraperName);
     scraperContainer.appendChild(scraperLogoContainer);
     scraperLogoContainer.appendChild(scraperLogo);
     scraperContainer.appendChild(scraperInfo);
+    scraperInfo.appendChild(scraperDescription);
+
     scraperInfo.appendChild(scraperLinkContainer);
     scraperLinkContainer.appendChild(scraperLink);
+    scraperLinkContainer.appendChild(scraperLinkSite);
 
     onpage_scrapere.push(scraperContainer);
   });
-}
-
-function checkFileExist(url, container) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("head", url, false);
-  xhr.send();
-  if (xhr.status == 404) {
-    container.classList.add("add-class");
-  }
 }
